97ccf99f16935effb3d2a552d0e5d39acf2c723e
Kyle Macfarlan <kyle.macfarlan@gmail.com>
2018-09-22 16:46:07 -0500

    Fix conan file

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,6 @@
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, CMake
 from conans.tools import load
-import re, os
+import re
 def get_version():
     try:
         content = load("SuiteSparse/SuiteSparse_config/SuiteSparse_config.h")
@@ -8,7 +8,7 @@ def get_version():
         version_minor = re.search(r"#define SUITESPARSE_SUB_VERSION (.*)", content).group(1)
         version_update = re.search(r"#define SUITESPARSE_SUBSUB_VERSION (.*)", content).group(1)
         return version_major.strip() + "." + version_minor.strip() + "." + version_update.strip()
-    except Exception as e:
+    except Exception:
         return None
 
 class SuiteSparseConan(ConanFile):
@@ -39,6 +39,7 @@ class SuiteSparseConan(ConanFile):
         self.copy("*.h", dst="include", src="SuiteSparse/CAMD/Include")
         self.copy("*.h", dst="include", src="SuiteSparse/CCOLAMD/Include")
         self.copy("*.h", dst="include", src="SuiteSparse/CHOLAMD/Include")
+        self.copy("*.h", dst="include", src="SuiteSparse/CHOLMOD/Include")
         self.copy("*.h", dst="include", src="SuiteSparse/COLAMD/Include")
         self.copy("*.h", dst="include", src="SuiteSparse/CSparse/Include")
         self.copy("*.h", dst="include", src="SuiteSparse/CXSparse/Include")
@@ -47,6 +48,22 @@ class SuiteSparseConan(ConanFile):
         self.copy("*.h", dst="include", src="SuiteSparse/SPQR/Include")
         self.copy("*.h", dst="include", src="SuiteSparse/UMFPACK/Include")
         self.copy("*.h", dst="include", src="SuiteSparse/SuiteSparse_config")
+
+        self.copy("*.hpp", dst="include", src="SuiteSparse/AMD/Include")
+        self.copy("*.hpp", dst="include", src="SuiteSparse/BTF/Include")
+        self.copy("*.hpp", dst="include", src="SuiteSparse/CAMD/Include")
+        self.copy("*.hpp", dst="include", src="SuiteSparse/CCOLAMD/Include")
+        self.copy("*.hpp", dst="include", src="SuiteSparse/CHOLMOD/Include")
+        self.copy("*.hpp", dst="include", src="SuiteSparse/CHOLAMD/Include")
+        self.copy("*.hpp", dst="include", src="SuiteSparse/COLAMD/Include")
+        self.copy("*.hpp", dst="include", src="SuiteSparse/CSparse/Include")
+        self.copy("*.hpp", dst="include", src="SuiteSparse/CXSparse/Include")
+        self.copy("*.hpp", dst="include", src="SuiteSparse/KLU/Include")
+        self.copy("*.hpp", dst="include", src="SuiteSparse/LDL/Include")
+        self.copy("*.hpp", dst="include", src="SuiteSparse/SPQR/Include")
+        self.copy("*.hpp", dst="include", src="SuiteSparse/UMFPACK/Include")
+        self.copy("*.hpp", dst="include", src="SuiteSparse/SuiteSparse_config")
+
         self.copy("*.lib", dst="lib", keep_path=False)
         self.copy("*.dll", dst="bin", keep_path=False)
         self.copy("*.dylib*", dst="lib", keep_path=False)
